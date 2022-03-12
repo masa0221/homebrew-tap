1e1fcc84417a0e287cb8be314b59881f32ce08f9
Masashi Tsuru <0221masa@gmail.com>
2022-03-12 18:52:01 +0900

    Add formula of oauth2-dev-tool

---
+++ Formula/oauth2-dev-tool.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Oauth2DevTool < Formula
+  desc "This is a tool for OAuth 2.0 development"
+  homepage "https://github.com/masa0221/oauth2-dev-tool"
+  url "https://github.com/masa0221/oauth2-dev-tool/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "ee47bc94654e3b7b4ef1d32227c6e8c285392184a23d03bbb9f91972a3383c61"
+  license "MIT"
+
+  def install
+    bin.install "oauth"
+  end
+end
