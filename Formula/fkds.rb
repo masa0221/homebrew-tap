07255ff87729c06d1d8e751607a9abff83643296
Masashi Tsuru <0221masa@gmail.com>
2022-08-27 17:00:31 +0900

    Fixed description to not end with a full stop

--- Formula/fkds.rb
+++ Formula/fkds.rb
@@ -1,5 +1,5 @@
 class Fkds < Formula
-  desc "This is a things for generating speech bubble."
+  desc "This is a things for generating speech bubble"
   homepage "https://github.com/masa0221/fkds"
   url "https://github.com/masa0221/fkds/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "dbd4804bbba1961d89d6b8c4bbddead6f912548c35a980682a694edf030a1ae3"
