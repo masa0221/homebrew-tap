860d6b721b3f4a488a566e694d833df577eb2506
Masashi Tsuru <0221masa@gmail.com>
2023-06-15 09:34:56 +0900

    Fix style

--- Formula/eks-admin.rb
+++ Formula/eks-admin.rb
@@ -1,5 +1,5 @@
 class EksAdmin < Formula
-  desc "This tool is for operating EKS cluster."
+  desc "This tool is for operating EKS cluster"
   homepage "https://github.com/masa0221/eks-operations"
   url "https://github.com/masa0221/eks-operations/releases/download/v0.1.0/eks-admin"
   sha256 "ed7f098634491679e76d7688fd5c176d42914c54b1f978a62ebc8b0156789892"
